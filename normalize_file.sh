29a699e2f5b6be9575934726f999d43156dadf2a
doix <arek_bal@wp.pl>
2020-11-03 20:07:58 +0100

    Update normalize_file.sh

--- normalize_file.sh
+++ normalize_file.sh
@@ -3,16 +3,15 @@
 INPUT=$1
 OUTPUT=$2
 USE_LIMITER=$3
- if [ "$USE_LIMITER" == '-limiter' ] ; then
-LIMITER="-af alimiter=limit=0.83"
+if [ "$USE_LIMITER" == '-limiter' ] ; then
+ LIMITER="alimiter=limit=0.83,"
 else
-LIMITER=""
+ LIMITER="alimiter=limit=0.83,"
 fi
-ffmpeg -i "$INPUT" -af volumedetect -f null -y nul &> original.txt
+ffmpeg -hide_banner -i "$INPUT" -af volumedetect -f null -y nul &> original.txt
 grep "max_volume" original.txt > original.tmp
 sed -i 's|: -|=|' original.tmp
-if [ $? = 0 ]
- then
+if [ $? = 0 ] ; then
  sed -i 's| |\r\n|' original.tmp
  sed -i 's| |\r\n|' original.tmp
  sed -i 's| |\r\n|' original.tmp
@@ -24,8 +23,32 @@ if [ $? = 0 ]
     GAIN_LEVEL="0"
  fi
  gain="$GAIN_LEVEL"dB
+ echo "$gain"
  rm "$OUTPUT"
- ffmpeg -i "$INPUT" -af "volume=$gain" $LIMITER -af silenceremove=0:0:0:-1:5:-92dB -metadata comment="" -codec:a libmp3lame -b:a 320k "$OUTPUT"
+ # -af "alimiter=limit=0.83,silenceremove=0:0:0:-1:5:-92dB"
+ ffmpeg -hide_banner -i "$INPUT" -filter_complex "volume=$gain, alimiter=limit=0.93:5:50" -metadata comment="" "$INPUT".tmp-limited.wav 
+  
+ ffmpeg -hide_banner -i "$INPUT".tmp-limited.wav -af volumedetect  -f null -y nul &> limitation_phase.txt 
  
- ffmpeg -i "$OUTPUT" -af volumedetect -f null -y nul &> result.txt
+ grep "max_volume" limitation_phase.txt > limitation_phase.tmp
+ sed -i 's|: -|=|' limitation_phase.tmp
+
+ sed -i 's| |\r\n|' limitation_phase.tmp
+ sed -i 's| |\r\n|' limitation_phase.tmp
+ sed -i 's| |\r\n|' limitation_phase.tmp
+ sed -i 's| |\r\n|' limitation_phase.tmp
+ grep "max_volume" limitation_phase.tmp > limitation_phase2.tmp
+ sed -i 's|max_volume=||' limitation_phase2.tmp
+ GAIN_LEVEL=$(cat "./limitation_phase2.tmp")
+ if [ "$GAIN_LEVEL" == "0" ] ; then
+    GAIN_LEVEL="0"
+ fi
+ gain="$GAIN_LEVEL"dB
+ echo "$gain" 
+ 
+ ffmpeg -hide_banner -i "$INPUT".tmp-limited.wav -filter_complex "volume=$gain, silenceremove=0:0:0:-1:5:-92dB" -metadata comment="" -codec:a libmp3lame -b:a 320k "$OUTPUT" 
+ 
+ rm "$INPUT".tmp-limited.wav
+ 
+ ffmpeg -hide_banner -i "$OUTPUT" -af volumedetect  -f null -y nul &> result.txt  
 fi
